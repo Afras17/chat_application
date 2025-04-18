bcc6cbcbe9fb91cc59f5f665c05ddf4aba008446
Afras Rahim <afrasrahim11@gamil.com>
2025-04-18 14:27:24 +0530

    auth routes completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, logout, updateProfile } from '../controllers/auth.controller.js'; // Import the controller functions
+import { signup, login, logout, updateProfile, checkAuth } from '../controllers/auth.controller.js'; // Import the controller functions
 import { protectRoute } from '../middleware/auth.middleware.js'; // Import the middleware for protecting routes
 
 const router = express.Router();
@@ -8,7 +8,9 @@ router.post('/signup', signup); // Use the signup function from the controller
 router.post('/login', login); // Use the login function from the controller
 router.post('/logout', logout); // Use the logout function from the controller
 
-router.put('/update-profile', protectRoute, updateProfile)
+router.put('/update-profile', protectRoute, updateProfile);
+
+router.get('/check', protectRoute, checkAuth); // Use the checkAuth function from the controller
 
 export default router;
 
